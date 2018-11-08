04b5a1b9592f7bbaffc2f0f929f6d5a08454d11f
juazouz <juazouz@student.42.fr>
2018-11-08 16:52:00 +0100

    Fix ft_memmove

--- srcs/memory/ft_memmove.c
+++ srcs/memory/ft_memmove.c
@@ -6,19 +6,39 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:13:25 by juazouz           #+#    #+#             */
-/*   Updated: 2018/11/05 17:12:30 by juazouz          ###   ########.fr       */
+/*   Updated: 2018/11/08 16:50:38 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t num)
+static void	memcpy_backward(void *dst, const void *src, size_t num)
 {
-	void *buf;
+	size_t	i;
 
-	buf = malloc(num);
-	ft_memcpy(buf, src, num);
-	ft_memcpy(dst, buf, num);
-	free(buf);
+	if (num == 0)
+	{
+		return ;
+	}
+	i = num - 1;
+	while (1)
+	{
+		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
+		if (i <= 0)
+			return ;
+		i--;
+	}
+}
+
+void		*ft_memmove(void *dst, const void *src, size_t num)
+{
+	if (dst < src)
+	{
+		ft_memcpy(dst, src, num);
+	}
+	else
+	{
+		memcpy_backward(dst, src, num);
+	}
 	return (dst);
 }
