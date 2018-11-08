83bfc3149c2dd2dd3372152d732f61a11d059215
juazouz <juazouz@student.42.fr>
2018-11-08 16:59:12 +0100

    Fix ft_strchr ft_strrchr

--- srcs/string/ft_strchr.c
+++ srcs/string/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:32:33 by juazouz           #+#    #+#             */
-/*   Updated: 2018/11/05 16:33:24 by juazouz          ###   ########.fr       */
+/*   Updated: 2018/11/08 16:55:03 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ char	*ft_strchr(const char *str, int character)
 	size_t	i;
 
 	i = 0;
-	while (((unsigned char*)str)[i] != '\0')
+	while (1)
 	{
 		if (((unsigned char*)str)[i] == (unsigned char)character)
 			return ((char*)(str + i));
+		if (((unsigned char*)str)[i] == '\0')
+			return (NULL);
 		i++;
 	}
-	return (NULL);
 }
--- srcs/string/ft_strrchr.c
+++ srcs/string/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 17:08:20 by juazouz           #+#    #+#             */
-/*   Updated: 2018/11/05 17:08:30 by juazouz          ###   ########.fr       */
+/*   Updated: 2018/11/08 16:57:43 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ char	*ft_strrchr(const char *str, int character)
 
 	found = NULL;
 	i = 0;
-	while (((char*)str)[i] != '\0')
+	while (1)
 	{
 		if (((unsigned char*)str)[i] == (unsigned char)character)
 			found = (char*)(str + i);
+		if (((char*)str)[i] == '\0')
+			return (found);
 		i++;
 	}
-	return (found);
 }
