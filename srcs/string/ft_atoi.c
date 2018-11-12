97a70a08032848d058dc1c5f1ff85c763f67956e
juazouz <juazouz@student.42.fr>
2018-11-12 16:05:47 +0100

    ft_atoi fix

--- srcs/string/ft_atoi.c
+++ srcs/string/ft_atoi.c
@@ -6,36 +6,47 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/08 14:20:31 by juazouz           #+#    #+#             */
-/*   Updated: 2018/10/26 19:27:57 by juazouz          ###   ########.fr       */
+/*   Updated: 2018/11/12 15:59:40 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+static int	get_sign(char *str, int *i)
+{
+	if (str[(*i)] == '-')
+	{
+		(*i)++;
+		return (-1);
+	}
+	if (str[(*i)] == '+')
+	{
+		(*i)++;
+	}
+	return (1);
+}
 
 int			ft_atoi(char *str)
 {
-	int digit;
-	int result;
-	int i;
-	int sign;
+	int		digit;
+	long	result;
+	int		i;
+	int		sign;
 
 	result = 0;
 	i = 0;
 	sign = 1;
 	while (ft_isspace(str[i]))
 		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
-		i++;
+	sign = get_sign(str, &i);
 	while (str[i] != '\0' && ft_isdigit(str[i]))
 	{
 		result *= 10;
 		digit = str[i] - '0';
 		result += digit;
+		if (result < 0)
+			return (-1);
 		i++;
 	}
 	return (sign * result);
